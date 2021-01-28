d0294ec2ba5aabb60874e3e0a0f3f6fc842db274
Tatsuya Maeta <tat.mae084@gmail.com>
2021-01-28 13:02:15 +0900

    first commit

---
+++ Sec01/Sec01/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Sec01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("HELLO");
+            Console.Write("WORLD");
+
+        }
+    }
+}
+
