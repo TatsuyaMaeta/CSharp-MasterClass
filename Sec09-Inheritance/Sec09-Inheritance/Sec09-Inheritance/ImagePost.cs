be05c4d5a08431c3b0e3e62149006e20960ff5c1
Tatsuya Maeta <tat.mae084@gmail.com>
2021-02-27 14:57:49 +0900

    fixed files

--- Sec09-Inheritance/Sec09-Inheritance/Sec09-Inheritance/ImagePost.cs
+++ Sec09-Inheritance/Sec09-Inheritance/Sec09-Inheritance/ImagePost.cs
@@ -1,10 +1,32 @@
 ﻿using System;
 namespace Sec09_Inheritance
 {
-    public class ImagePost
+    class ImagePost:Post
     {
+        public string ImageURL{ get; set; }
+
         public ImagePost()
         {
+
         }
+
+        public ImagePost(string title, string sendByUsername, string imageURL, bool isPublic)
+        {
+            //以下のプロパティとgetNextIDメソッドはPostから継承されています。
+            this.ID = GetNextID();
+            this.Title = title;
+            this.SendByUsername = sendByUsername;
+            this.Ispublic = isPublic;
+
+            //ここから下のImageUrlはImagePostのメンバだが、Postに存在するメンバではない(ImagePost固有)
+            this.ImageURL = imageURL;
+
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0} - {1} - {2} by {3}", this.ID, this.Title, this.SendByUsername,this.ImageURL);
+        }
+
     }
 }
--- Sec09-Inheritance/Sec09-Inheritance/Sec09-Inheritance/Program.cs
+++ Sec09-Inheritance/Sec09-Inheritance/Sec09-Inheritance/Program.cs
@@ -8,8 +8,13 @@ namespace Sec09_Inheritance
         {
             Post post1 = new Post("thanks for the birthday wishes.", true, "Denis");
             Console.WriteLine(post1.ToString());
-            Console.ReadKey();
+            //Console.ReadKey();
 
+            ImagePost imagepost1 = new ImagePost("check out my new shoes", "Tatsuya",
+                                                    "http://image.com", true);
+
+            Console.WriteLine(imagepost1.ToString());
+            Console.ReadKey();
         }
     }
 }
